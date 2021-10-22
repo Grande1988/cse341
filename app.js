fab43bc8da9b648d6ecb18d739e276eb9ea29252
Grande1988 <71180666+Grande1988@users.noreply.github.com>
2021-10-21 21:49:18 -0600

    added css

--- app.js
+++ app.js
@@ -8,55 +8,29 @@ const errorController = require("./controllers/error");
 const User = require("./models/user");
 
 const app = express();
-const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: "sessions",
-});
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+const PORT = process.env.PORT || 5000;
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const authRoutes = require("./routes/auth");
+const corsOptions = {
+  origin: "https://morning-plateau-13760.herokuapp.com",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-);
+const options ={
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+  family: 4
+};
 
-app.use(express.static(path.join(__dirname, "public")));
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://reddgl:<password>@cluster0.sll31.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-app.use(
-  session({
-    secret: "my secret",
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-  })
-);
+//const app = express();
 
-app.use((req, res, next) => {
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
-app.use(authRoutes);
-
-app.use(errorController.get404);
-
-mongoConnect(() => {
-  app.listen(5000);
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
---
+++ routes/admin.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+
+app.use('/add-product', (req, red, next) )
+router.post
+
+module.exports = router;
